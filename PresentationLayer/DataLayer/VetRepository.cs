7526cc754e204f8ed7ecc90f88caef46227b4089
dpetric70 <70-2018@ftn.edu.rs>
2020-12-03 12:49:19 +0100

    Kreiran DataLayer sa odgovarajucim klasama i metodama

---
+++ PresentationLayer/DataLayer/VetRepository.cs
@@ -0,0 +1,54 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class VetRepository
+    {
+        public List<Vet> GetAllVets()
+        {
+            List<Vet> vets = new List<Vet>();
+            using (SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText = "SELECT * FROM Vets;";
+
+                sqlConnection.Open();
+
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                while (sqlDataReader.Read())
+                {
+
+                    int Id = sqlDataReader.GetInt32(0);
+                    string FullName = sqlDataReader.GetString(1);
+                    string Speciality = sqlDataReader.GetString(2);
+                    int YearsExperience = sqlDataReader.GetInt32(3);
+
+                    vets.Add(new Vet(Id, FullName, Speciality, YearsExperience));
+                }
+            }
+
+            return vets;
+        }
+        public int InsertVet(Vet v)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText =
+                    string.Format("INSERT INTO Vets VALUES('{0}', '{1}'. {2})", v.FullName, v.Speciality, v.YearsExperience);
+
+                sqlConnection.Open();
+                return sqlCommand.ExecuteNonQuery();
+            }
+        }
+    }
+}
